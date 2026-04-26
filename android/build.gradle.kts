2242c157bb97dcadd7b7d94a63706c8648b60e89
sleet0922 <sleet0528@outlook.com>
2026-04-27 00:47:46 +0800

    1

---
+++ android/build.gradle.kts
@@ -0,0 +1,43 @@
+// 1. 必须添加 buildscript 块，否则找不到 Gradle 插件
+buildscript {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        google()
+        mavenCentral()
+    }
+}
+
+allprojects {
+    repositories {
+        // 阿里云国内镜像
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+
+        // 官方备用
+        google()
+        mavenCentral()
+    }
+}
+
+// 以下是你原来的 build 路径重定向逻辑，保持不变即可
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
